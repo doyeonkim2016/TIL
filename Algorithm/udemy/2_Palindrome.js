7ab9c2b2d362e3b1d1842f7a3a897901646e19e1
Kim Do Yeon <doyeon9502s2@gmail.com>
2019-12-21 15:28:09 +0800

    Create 2_Palindrome.js

---
+++ Algorithm/udemy/2_Palindrome.js
@@ -0,0 +1,24 @@
+// --- Directions
+// Given a string, return true if the string is a palindrome
+// or false if it is not.  Palindromes are strings that
+// form the same word if it is reversed. *Do* include spaces
+// and punctuation in determining if the string is a palindrome.
+// --- Examples:
+//   palindrome("abba") === true
+//   palindrome("abcdefg") === false
+
+//can use .every function in js
+//Array.every((val)=> val > 5)  is every value greater than 5?
+// make all letters in array then 
+function palindrome(str) {
+    return str.split('').every((char,i)=> {
+        return char === str[str.length-i-1];
+    });
+}
+
+module.exports = palindrome;
+
+//function palindrome(str){
+//    const reversed = str.split('').reverse().join('');
+//    return str === reversed;
+//}
