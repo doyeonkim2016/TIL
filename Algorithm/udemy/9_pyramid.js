1f6b18a24b5fcab4292b85bb3099999ef44e74fa
Kim Do Yeon <doyeon9502s2@gmail.com>
2020-01-02 23:51:29 +0800

    Create 9_pyramid.js

---
+++ Algorithm/udemy/9_pyramid.js
@@ -0,0 +1,40 @@
+ // --- Directions
+// Write a function that accepts a positive number N.
+// The function should console log a pyramid shape
+// with N levels using the # character.  Make sure the
+// pyramid has spaces on both the left *and* right hand sides
+// --- Examples
+//   pyramid(1)
+//       '#'
+//   pyramid(2)
+//       ' # '
+//       '###'
+//   pyramid(3)
+//       '  #  '
+//       ' ### '
+//       '#####'
+// From 0 to n (iterate through rows)
+//     Create an empty string 'level'
+//     From 0 to ??? (Columns)
+//         If the column should have a #
+//             Add a # to "Level"
+//         else
+//             Add a space to "level"
+
+function pyramid(n) {
+    const midpoint = Math.floor((2*n-1)/2);
+    for(let row =0; row < n; row ++){
+        let level ='';
+        for (let column = 0; column <2*n-1;column++){
+           if(midpoint - row <= column && midpoint +row >= column){
+               level += '#';
+           }else{
+               level+= ' ';
+           }
+
+        }
+    }
+
+}
+
+module.exports = pyramid;
