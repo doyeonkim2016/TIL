0294944a3e41cb4df8185eff1ed6bc346b16940a
Kim Do Yeon <doyeon9502s2@gmail.com>
2020-06-27 14:41:21 +0900

    Create 3.AppleandOrange.py

---
+++ Algorithm/3.AppleandOrange.py
@@ -0,0 +1,6 @@
+#The true test of problem solving: when one realizes that time and memory aren't infinite.
+#http://hr.gs/4jmzs
+
+#For this question you dont have to use count and for loop
+
+Print(sum[1 for x in apples if x+a >= s and x+a <= t]))
