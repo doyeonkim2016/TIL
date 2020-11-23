4f74d1e214b43eb4c737f7d80e77046f49aaa3ab
Kim Do Yeon <doyeon9502s2@gmail.com>
2020-11-23 22:38:42 +0900

    Create OOP1.java

---
+++ Java/Geeks/OOP1.java
@@ -0,0 +1,29 @@
+//Classes and Objects in Java
+
+//1. Modifiers
+// A class can be ppublic or has default access
+
+//2. class Keyword
+//class keyword is used to create a class
+
+//3.Class name 
+// The name should begin with a initial letter( capitalized by convention)
+
+//4. Superclass
+// The name of the class's parent (superclass),if any, preceded by the keyword extends. A class can only extend one parent
+
+//5. Interfaces(if any)
+//A comma-separated list of interfaces implemented by the class, if any, preceded by the keyword implements.
+//A class can implement more than one interface
+
+
+//Creating object of a class
+// USE NEW KEYWORD
+
+// Creating object of class Test
+Test t = new Test();
+
+//Using clone() method
+Test t1 = new Test();
+
+Test t2 = (Test)t1.clone();
