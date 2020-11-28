1d3c5e2ab879bc67e65da0d1b979d35f37796246
Kim Do Yeon <doyeon9502s2@gmail.com>
2020-11-28 23:31:32 +0900

    Create React01.jsx

---
+++ React/React01.jsx
@@ -0,0 +1,15 @@
+/*
+I.React is an Open Source view library.It is a great tool to render the User Interface of mdern web application
+
+React uses syntax extension of JavaScript called JSX that allows you to write HTML directly within JS.
+For instance, because JSX is syntactic extension of JavaScript, you can actualy write JS directly within JSX. 
+
+ReactDOM.render(JSX, document.getElementById('root'))
+
+This function call is what places your JSX into REacts' own light weight representation of the DOM.
+React then uses snapshots of its own DOM to optimise updating only specific parts of actual DOM.
+
+II. Create a Complex JSX element
+JSX can represent more complex HTML as well.
+
+
