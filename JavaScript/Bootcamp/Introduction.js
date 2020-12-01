274a21a0ab1b560840395b36371560b6dff265a3
Kim Do Yeon <doyeon9502s2@gmail.com>
2020-12-01 22:52:00 +0900

    Create Introduction.js

---
+++ JavaScript/Bootcamp/Introduction.js
@@ -0,0 +1,14 @@
+/*
+Web Development Bootcamp
+Console is used to test a line of code
+Snippet is to test the whole script and execute.
+Either single quote or double quote is fine.
+*/
+
+alert("Hello");
+alert("World!");
+
+var myName ="DoYeon";
+alert(myName);
+
+var yourName =Prompt("What is Your Name?");
