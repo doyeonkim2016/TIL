b40c6da00412981c537a1b58d57e956751bd2054
Kim Do Yeon <doyeon9502s2@gmail.com>
2021-05-12 23:50:43 +0900

    Create BasicEvaluation.java

---
+++ Java/GradingSystem/Grade/BasicEvaluation.java
@@ -0,0 +1,14 @@
+package grade;
+
+public class BasicEvaluation implements GradeEvaluation[
+  
+  
+  @Override
+  public String getGrade(int point){
+    String grade;
+    
+    if(point >=90 && point <=100){
+      grade ="A";
+    }
+    
+ }
