68220dfab28aae7a64d981c0fc5fb1f6f7ea207f
Kim Do Yeon <doyeon9502s2@gmail.com>
2021-05-12 23:51:11 +0900

    Create MajorEvaluation.java

---
+++ Java/GradingSystem/Grade/MajorEvaluation.java
@@ -0,0 +1,14 @@
+package grade;
+
+public class MajorEvaluation implements GradeEvaluation[
+  
+  
+  @Override
+  public String getGrade(int point){
+    String grade;
+    
+    if(point >=80 && point <=100){
+      grade ="A";
+    }
+    
+ }
