cd5cb05a13fab7d4dd7a2f3f5b57192640dfeea7
Kim Do Yeon <doyeon9502s2@gmail.com>
2021-05-15 23:21:41 +0900

    Create String.java

---
+++ Java/Geeks/Strings/String.java
@@ -0,0 +1,29 @@
+//Creating a String
+
+String s = "Geeks for Geeks";
+
+//String Buffer
+/*
+
+StringBuffer is a peer classs of String that provides much of funcionality of strings.
+The string represents fixed -length,immutable character sequences while StringBuffer represents growable and writable character sequences
+
+*/
+
+StringBuffer s = new StringBuffer("GeeksForGeeks");
+
+/*
+
+StringBuilder in Java represents a mutable sequence of characters. Since the String Class in Java creates an immutable sequence of characters,
+the StringBuilder class provides an alternate to String Class, as it creates a mutable sequence of characters
+
+*/
+StringBuilder str = new StringBuilder();
+str.append("GFG");
+
+//String Methods
+
+
+  
+ 
+  
