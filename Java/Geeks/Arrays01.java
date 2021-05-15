3381a0e411417c7f2d02edb79a5ee1574f680fc7
Kim Do Yeon <doyeon9502s2@gmail.com>
2021-05-15 23:51:48 +0900

    Create Arrays01.java

---
+++ Java/Geeks/Arrays01.java
@@ -0,0 +1,18 @@
+//Cloning of Arrays
+
+class Test{
+  
+  public static void main(String args[]){
+    int intArray[] ={1,2,3};
+    int cloneArray[] =intArray.clone();
+    
+    System.out.println(intArray ==cloneArray);
+    
+    for(int i = 0 ; i < cloneArray.length ; i++){
+      System.out.print(cloneArray[i]);
+    
+    }
+  }
+}
+
+    
