bb0e9730c7d7f13c345b4ced404ac54fb20ec796
Kim Do Yeon <doyeon9502s2@gmail.com>
2021-05-16 14:45:33 +0900

    Create 3-3.py

---
+++ Algorithm/Greedy/3-3.py
@@ -0,0 +1,12 @@
+#N,M 을 공백으로 구분하여 입력받기
+
+ n,m = map(int, input.split())
+  
+ result = 0;
+
+for i in range(n):
+    data = list(map(int, input().split()))
+    min_value = min(data)
+    result = max(result,min_value)
+    
+print(result);
