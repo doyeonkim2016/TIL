90ed27be798709bb36cbcaf04bd7e701da0d002a
Kim Do Yeon <doyeon9502s2@gmail.com>
2021-05-30 22:46:01 +0900

    Create Interview01.java

---
+++ Algorithm/Greedy/Interview01.java
@@ -0,0 +1,137 @@
+//https://www.hackerrank.com/challenges/minimum-absolute-difference-in-an-array/problem?h_l=interview&playlist_slugs%5B%5D%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D%5B%5D=greedy-algorithms&isFullScreen=true
+
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'minimumAbsoluteDifference' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+  //O(N^2)
+    public static int minimumAbsoluteDifference(List<Integer> arr) {
+    // Write your code here
+        int result = 0;
+        int min = Integer.MAX_VALUE;
+        int pos =0;
+        for(int i =  0; i < arr.size()-1 ; i++){
+            for(int j = i+1 ; j <arr.size() ;j++){
+                result = Math.abs(arr.get(i)- arr.get(j));
+                System.out.println(result);
+                System.out.println(min);
+                if(result < min){
+                    min = result;
+                }
+            }    
+        }
+    
+        return min;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.minimumAbsoluteDifference(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
+
+//===========================================================================
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'minimumAbsoluteDifference' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int minimumAbsoluteDifference(List<Integer> arr) {
+    // Write your code here
+        
+        
+        Collections.sort(arr);
+        int min = Integer.MAX_VALUE;
+        int result =0;
+        
+        for(int i= 0; i < arr.size()-1 ; i ++){
+            if(i < arr.size()-1){
+                result = Math.abs(arr.get(i+1)- arr.get(i));
+                    System.out.println(result);
+                if(result <min){
+                    min =result;
+                }
+                    
+            }
+        }   
+        
+        
+        
+       
+    
+        return min;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.minimumAbsoluteDifference(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
